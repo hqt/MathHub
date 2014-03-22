2d48631f4344a2d57644fba60d5805ac4c10a694
HaiNNT <hainnt1302@gmail.com>
2014-03-22 11:51:51 +0700

    add signalR DemoCode

---
+++ Research/DemoCode/SignalRChat/SignalRChat/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            TransData data = new TransData();
+            data.Name = name;
+            data.Age = 21;
+            data.BirthDay = DateTime.Now;
+            data.Message = message;
+
+            string jsonData = JsonConvert.SerializeObject(data);
+
+            Clients.All.broadcastMessage(name, jsonData);
+            
+        }
+    }
+
+    public class TransData
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Research/DemoCode/SignalRChat/SignalRChat/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+
+}
